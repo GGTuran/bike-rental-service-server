a11fd7caa3b61a5ac75c30a17e9ecbbbfcaa7b56
GGTuran <tayyabturan67@gmail.com>
2024-06-13 10:03:45 +0600

    added castError

--- src/app/errors/AppError.ts
+++ src/app/errors/AppError.ts
@@ -1,10 +1,8 @@
 class AppError extends Error{
-    public statusCode:number;
-
-    constructor(statusCode:number, message:string, stack=''){
+    public statusCode :number;
+    constructor(statusCode:number, message:string, stack = ''){
         super(message);
         this.statusCode = statusCode;
-
         if(stack){
             this.stack = stack;
         } else{
